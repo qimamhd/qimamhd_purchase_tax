d63f120afaf461849a6c1725e661aa9d01cfa2dc
qimamhd <106440499+qimamhd@users.noreply.github.com>
2025-10-13 15:51:23 +0300

    update

--- models/account_move.py
+++ models/account_move.py
@@ -12,23 +12,25 @@ class journal_closed(models.Model):
 
     
    
-    @api.onchange('company_id')
+    @api.onchange('tax_expense_flag')
     def set_domain_purchase_journal(self):
         if self.type in ('in_refund','in_invoice') and self.tax_expense_flag:
             journals = self.env['account.journal'].search([('tax_expense_journal_id','=', True)])
             if journals:
                 return {'domain': {'journal_id': [('id', 'in', journals.ids)]}}
             else:
-                return {'domain': {'journal_id': False}}
+
+                journals = self.env['account.journal'].search([('type','=','purchase')])
+
+                return {'domain': {'journal_id':  [('id', 'in', journals.ids)]}}
 
 
         
-    @api.onchange('company_id')
+    @api.onchange('tax_expense_flag')
     def _default_values(self):
         # vals = super(purchase_order_getDefault, self)._default_values()
         if self.type in ('in_refund','in_invoice') and self.tax_expense_flag:
             journals = self.env['account.journal'].search([('tax_expense_journal_id','=', True)],limit=1)
              
             self.update({'journal_id':journals.id}) 
-            
-       
+        
